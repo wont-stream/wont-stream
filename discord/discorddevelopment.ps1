8a88c5b8eb9c0b315807669356623eb1bfe34497
Unstream <143244075+wont-stream@users.noreply.github.com>
2024-01-01 21:02:21 -0500

    Update and rename discorddevelopment.ps1 to discorddevelopment.bat

--- discord/discorddevelopment.ps1
+++
@@ -1,8 +0,0 @@
-$url = "https://discord.com/api/downloads/distributions/app/installers/latest?channel=development&platform=win&arch=x64"
-$outputPath = "$env:TEMP\DiscordInstaller.exe"
-
-# Download the file
-Invoke-WebRequest -Uri $url -OutFile $outputPath
-
-# Run the downloaded file
-Start-Process -FilePath $outputPath -Wait
